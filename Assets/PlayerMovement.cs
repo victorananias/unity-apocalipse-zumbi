f0a962ba7f42f9dcb99eb5e2d3fa3db91b125de2
Victor <victor@ananias.dev>
2020-01-30 22:35:31 -0300

    initial commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float PlayerSpeed = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var aixosX = Input.GetAxis("Horizontal");
+        var aixosY = Input.GetAxis("Vertical");
+        
+        var newPosition = new Vector3(aixosX, 0, aixosY);
+        
+        transform.Translate(newPosition * Time.deltaTime * PlayerSpeed);
+    }
+}
