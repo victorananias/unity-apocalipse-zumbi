2e28c96ea1294403e4ec65fadaaa718e9da13336
Victor <victor@ananias.dev>
2020-02-11 22:54:02 -0300

    criando tiro da arma

---
+++ Assets/Scripts/GunController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    public GameObject Bala;
+    
+    void FixedUpdate()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(Bala, transform.position, transform.rotation);
+        }
+    }
+}
