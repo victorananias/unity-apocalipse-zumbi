c4f12c0a77d695356799885ad05d1130e970cb3e
Victor <victor@ananias.dev>
2020-02-16 10:47:22 -0300

    animação de ataque do zombie adicionada

--- Assets/Scripts/ZombieMovement.cs
+++ Assets/Scripts/ZombieMovement.cs
@@ -6,23 +6,28 @@ public class ZombieMovement : MonoBehaviour
 {
     public GameObject Jogador;
     public float Velocidade = 5;
+
     void Start()
     {
-        
     }
 
     void FixedUpdate()
     {
         var distancia = Vector3.Distance(Jogador.transform.position, transform.position);
 
-        if (distancia <= 2.3)
-        {
-            return;
-        }
-        
         var direcao = Jogador.transform.position - transform.position;
-        GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + direcao.normalized * Velocidade * Time.deltaTime);
         var novaRotacao = Quaternion.LookRotation(direcao);
         GetComponent<Rigidbody>().MoveRotation(novaRotacao);
+
+        if (distancia > 2.3)
+        {
+            GetComponent<Rigidbody>()
+                .MovePosition(GetComponent<Rigidbody>().position + direcao.normalized * Velocidade * Time.deltaTime);
+            GetComponent<Animator>().SetBool("Atacando", false);
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("Atacando", true);
+        }
     }
 }
