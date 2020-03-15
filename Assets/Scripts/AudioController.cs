a4a4edf7172ef45787f3f89b338a6411a57cfa55
Victor <victor@ananias.dev>
2020-03-14 22:24:07 -0300

    musica de fundo adicionada

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    private AudioSource _audioSource;
+    private static AudioSource _sharedAudioSource;
+    
+    void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        _sharedAudioSource = _audioSource;
+    }
+}
